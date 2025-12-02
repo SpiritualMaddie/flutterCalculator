53086d0ab32cd8f6041cf51f54187bbd264878bf
Madde <MaddeL89@gmail.com>
2025-12-02 13:16:00 +0100

    tests created: basic arithmetic, order of operations, error handling.

---
+++ flutter_calc/test/calculation_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_calc/utils/calculate_methods.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+  group("calculate()", () {
+    test("basic arithmetic", () {
+      expect(calculate("1+1"), "2.0");
+      expect(calculate("2*5"), "10.0");
+    });
+
+    test("order of operations", () {
+      expect(calculate("2+3*4"), "14.0");
+    });
+
+    test("error handling", () {
+      expect(calculate("2+"), "Error!");
+      expect(calculate("8/0"), "Infinity");
+    });
+  });
+}
